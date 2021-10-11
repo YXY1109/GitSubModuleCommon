86b3b40e49389cdb899c1163d2685a09fa8ece79
522917595@qq.com <chengjie1004>
2021-10-11 09:07:36 +0800

    common init

---
+++ src/main/java/com/yxy/common/ToastUtils.java
@@ -0,0 +1,17 @@
+package com.yxy.common;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+public class ToastUtils {
+    public static void show(Context context, String msg) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
